03d795d3e1ddd7b5198a3158d0d81d17882cc05e
Paul Novichkov <paul.novichkov@yandex.com>
2018-08-05 20:16:07 +0200

    Add packaging system setup

---
+++ core/core-packages.el
@@ -0,0 +1,27 @@
+;;; core-packages.el --- Packaging system setup -*- lexical-binding: t; -*-
+
+;;; package.el
+(setq package-enable-at-startup nil
+      package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("gnu" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+
+;;; use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(setq use-package-always-defer t
+      use-package-always-ensure t)
+
+;;; Use consistent configuration paths.
+(use-package no-littering
+  :demand
+  :config
+  (setq custom-file (no-littering-expand-etc-file-name "custom.el")))
+
+(provide 'core-packages)
+;;; core-packages.el ends here
--- core/core.el
+++ core/core.el
@@ -1,4 +1,6 @@
 ;;; core.el --- Configuration core -*- lexical-binding: t; -*-
 
+(require 'core-packages (concat user-emacs-directory "core/core-packages.el"))
+
 (provide 'core)
 ;;; core.el ends here
