3b057707b060eff65f02ed4f7a9fd76d2f26431b
Paul Novichkov <paul.novichkov@yandex.com>
2018-11-02 19:34:27 +0100

    Optimize garbage collection

--- core/core.el
+++ core/core.el
@@ -1,5 +1,23 @@
 ;;; core.el --- Configuration core -*- lexical-binding: t; -*-
 
+;;; Optimize garbage collection.
+
+(defun my/increase-gc-threshold ()
+  (setq gc-cons-threshold 262144000)) ; 250 Mb.
+
+(defun my/restore-gc-threshold ()
+  (setq gc-cons-threshold 16777216)) ; 16 Mb.
+
+(if (or after-init-time noninteractive)
+    (my/restore-gc-threshold)
+  (my/increase-gc-threshold)
+  (add-hook 'emacs-startup-hook #'my/restore-gc-threshold))
+
+(add-hook 'minibuffer-setup-hook #'my/increase-gc-threshold)
+(add-hook 'minibuffer-exit-hook #'my/restore-gc-threshold)
+
+;;; Load core modules.
+
 (require 'core-packages (concat user-emacs-directory "core/core-packages.el"))
 (require 'core-keybindings (concat user-emacs-directory "core/core-keybindings.el"))
 (require 'core-emacs (concat user-emacs-directory "core/core-emacs.el"))
