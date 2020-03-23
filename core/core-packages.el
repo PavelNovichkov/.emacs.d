96d5681f773da6bda65df6bbe7f24eb68b0edb54
Pavel Novichkov <paul.novichkov@yandex.com>
2020-03-23 16:15:52 +0100

    Replace built-in package manager with straight

--- core/core-packages.el
+++ core/core-packages.el
@@ -1,21 +1,27 @@
 ;;; core-packages.el --- Packaging system setup -*- lexical-binding: t; -*-
 
-;;; package.el
-(setq package-enable-at-startup nil
-      package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("gnu" . "https://elpa.gnu.org/packages/")))
+;;; straight
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
 
-(package-initialize)
+(setq straight-check-for-modifications nil)
 
 ;;; use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(straight-use-package 'use-package)
 
-(require 'use-package)
+(setq straight-use-package-by-default t)
 
-(setq use-package-always-defer t
-      use-package-always-ensure t)
+(setq use-package-always-defer t)
 
 (setq use-package-compute-statistics t)
 
