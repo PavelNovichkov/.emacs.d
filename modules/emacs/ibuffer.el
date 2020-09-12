d32293922ad5bf602165a6db091fad1f6be29856
Pavel Novichkov <paul.novichkov@yandex.com>
2020-09-12 23:24:51 +0200

    Add ibuffer-projectile

--- modules/emacs/ibuffer.el
+++ modules/emacs/ibuffer.el
@@ -2,20 +2,11 @@
 
 (use-package ibuffer ; built-in
   :straight nil
+  :hook (ibuffer-mode . ibuffer-auto-mode)
   :config
-  (setq ibuffer-saved-filter-groups
-        '(("default"
-           ("dired" (mode . dired-mode))
-           ("org" (or (mode . org-mode) (name . "^Org Agenda$")))
-           ("shell" (or (mode . eshell-mode) (mode . shell-mode)))
-           ("mu4e" (name . "\*mu4e\*"))
-           ("programming" (mode . python-mode))
-           ("emacs" (or
-                     (name . "^\\*scratch\\*$")
-                     (name . "^\\*Messages\\*$")))
-           )))
-  (defun my/ibuffer-setup ()
-    (ibuffer-auto-mode 1)
-    (ibuffer-switch-to-saved-filter-groups "default"))
-  (add-hook 'ibuffer-mode-hook #'my/ibuffer-setup)
   (setq ibuffer-show-empty-filter-groups nil))
+
+(use-package ibuffer-projectile
+  :hook (ibuffer . ibuffer-projectile-set-filter-groups)
+  :config
+  (setq ibuffer-projectile-prefix ""))
