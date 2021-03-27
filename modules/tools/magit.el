c58ba04f4123ba3f2cab468a1a05f75d4ae1c605
Pavel Novichkov <paul.novichkov@yandex.com>
2021-03-27 21:00:44 +0100

    Remove evil-magit as it has become part of evil-collection

--- modules/tools/magit.el
+++ modules/tools/magit.el
@@ -5,17 +5,3 @@
   :commands magit-status
   :config
   (setq magit-section-visibility-indicator nil))
-
-(use-package evil-magit
-  :demand :after magit
-  :init
-  (setq evil-magit-use-z-for-folds t
-        evil-magit-want-horizontal-movement t)
-  :config
-  (general-define-key
-   :states evil-magit-state
-   :keymaps 'magit-mode-map
-   "?" #'evil-search-backward
-   "zz" #'evil-scroll-line-to-center
-   "zt" #'evil-scroll-line-to-top
-   "zb" #'evil-scroll-line-to-bottom))
