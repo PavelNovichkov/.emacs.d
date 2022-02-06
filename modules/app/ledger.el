658293bc8fdef48fe8a53598d1a41c726171f441
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2022-02-06 16:11:34 +0100

    Add ledger reports

--- modules/app/ledger.el
+++ modules/app/ledger.el
@@ -2,7 +2,13 @@
 
 (use-package ledger-mode
   :ensure-system-package ledger
-  :commands ledger-mode)
+  :commands ledger-mode
+  :config
+  (setq ledger-reports
+        '(("bal"            "%(binary) -f %(ledger-file) bal")
+          ("bal this month" "%(binary) -f %(ledger-file) bal -p %(month) -S amount")
+          ("bal this year"  "%(binary) -f %(ledger-file) bal -p 'this year'")
+          ("net worth"      "%(binary) -f %(ledger-file) bal Assets Liabilities")
+          ("account"        "%(binary) -f %(ledger-file) reg %(account)"))))
 
 ;; TODO: add evil-ledger.
-;; TODO: setup reports.
