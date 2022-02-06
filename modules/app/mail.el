77ca7babb58da31781b3d573cfab36621344726f
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2022-02-06 16:14:28 +0100

    Setup mu4e context policy

--- modules/app/mail.el
+++ modules/app/mail.el
@@ -49,6 +49,8 @@
               :action (lambda (docid msg target)
                         (mu4e~proc-move docid (mu4e~mark-check-target target) "-N"))))
   (setq mu4e-contexts (--map (apply 'make-mu4e-context it) my/mail-contexts))
+  (setq mu4e-context-policy 'pick-first
+        mu4e-compose-context-policy 'always-ask)
   ;; No auto-fill when composing a message.
   (add-hook 'mu4e-compose-mode-hook #'turn-off-auto-fill)
   ;; Turn on word wrap when viewing a message.
