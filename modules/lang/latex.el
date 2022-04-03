2b72f8cdd09363cb99c4d1e3c5a88922f79a188d
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2022-04-03 10:50:06 +0200

    Change cdlatex abbreviations

--- modules/lang/latex.el
+++ modules/lang/latex.el
@@ -79,21 +79,21 @@
            "\\langle ? \\rvert" cdlatex-position-cursor nil nil t)
           ("La" "Insert dynamically-sized left angle spinor"
            "\\left\\langle ? \\right\\rvert" cdlatex-position-cursor nil nil t)
-          ("ls" "Insert left square spinor"
+          ("lb" "Insert left square spinor"
            "[ ? \\rvert" cdlatex-position-cursor nil nil t)
-          ("Ls" "Insert dynamically-sized left square spinor"
+          ("Lb" "Insert dynamically-sized left square spinor"
            "\\left[ ? \\right\\rvert" cdlatex-position-cursor nil nil t)
           ("ra" "Insert right angle spinor"
            "\\lvert ? \\rangle" cdlatex-position-cursor nil nil t)
           ("Ra" "Insert dynamically-sized right angle spinor"
            "\\left\\lvert ? \\right\\rangle" cdlatex-position-cursor nil nil t)
-          ("rs" "Insert right square spinor"
+          ("rb" "Insert right square spinor"
            "\\lvert ? ]" cdlatex-position-cursor nil nil t)
-          ("Rs" "Insert dynamically-sized right square spinor"
+          ("Rb" "Insert dynamically-sized right square spinor"
            "\\left\\lvert ? \\right]" cdlatex-position-cursor nil nil t)
           ("lra" "Insert angle spinor bracket"
            "\\langle ? \\rangle" cdlatex-position-cursor nil nil t)
-          ("lrs" "Insert square spinor bracket"
+          ("lrb" "Insert square spinor bracket"
            "[ ? ]" cdlatex-position-cursor nil nil t)))
   (setq cdlatex-math-modify-alist
         '(( ?b "\\bm" nil t nil nil )
