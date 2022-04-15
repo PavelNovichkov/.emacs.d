ec53d4855fef711568068b7850b4f5a1168c7466
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2022-04-15 11:37:10 +0200

    Update color theme according to upstream

--- modules/ui/color-theme.el
+++ modules/ui/color-theme.el
@@ -3,8 +3,7 @@
 (use-package modus-themes
   :demand
   :config
-  (setq modus-themes-completions 'opinionated
-        modus-themes-diffs 'bg-only
+  (setq modus-themes-diffs 'bg-only
         modus-themes-lang-checkers '(text-also straight-underline)
         modus-themes-region '(bg-only no-extend)
         modus-themes-subtle-line-numbers t
