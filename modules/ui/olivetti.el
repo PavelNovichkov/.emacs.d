f98ac22b521e3d5b4938d1b766b0ee0cbeb09345
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2022-04-16 12:05:28 +0200

    Update olivetti config

--- modules/ui/olivetti.el
+++ modules/ui/olivetti.el
@@ -1,5 +1,6 @@
 ;;; ui/olivetti.el -*- lexical-binding: t; -*-
 
 (use-package olivetti
+  :hook ((text-mode prog-mode) . olivetti-mode)
   :config
-  (setq-default olivetti-body-width 80))
+  (setq-default olivetti-body-width 100))
