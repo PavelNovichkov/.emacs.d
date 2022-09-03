d330d290afbc2a7c6ffae73bd2db2a65c15f6a99
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2022-09-03 12:13:40 +0200

    Fix frame setup

--- modules/ui/frame.el
+++ modules/ui/frame.el
@@ -8,9 +8,9 @@
 
 (defconst my/fonts '("Fira Code Retina-12" . "Source Serif Pro-15"))
 
-(defun my/setup-fonts (frame)
+(defun my/setup-fonts ()
   "Setup fonts after GUI frame is created."
-  (set-face-attribute 'default frame :font (car my/fonts))
-  (set-face-attribute 'fixed-pitch frame :font (car my/fonts))
-  (set-face-attribute 'variable-pitch frame :font (cdr my/fonts)))
-(add-hook 'after-make-frame-functions #'my/setup-fonts)
+  (set-face-attribute 'default nil :font (car my/fonts))
+  (set-face-attribute 'fixed-pitch nil :font (car my/fonts))
+  (set-face-attribute 'variable-pitch nil :font (cdr my/fonts)))
+(add-hook 'server-after-make-frame-hook #'my/setup-fonts)
