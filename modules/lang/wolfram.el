530bcdac0fc5ef2683332eb05596069a3a910ffd
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2022-09-10 17:41:13 +0200

    Set fill-column in wolfram-mode

--- modules/lang/wolfram.el
+++ modules/lang/wolfram.el
@@ -2,7 +2,11 @@
 
 (use-package wolfram-mode
   :mode "\\.\\(wl\\|wls\\|m\\)\\'"
+  :hook (wolfram-mode . my/setup-wolfram-mode)
   :config
+  (defun my/setup-wolfram-mode ()
+    "Wolfram-mode-hook setup."
+    (setq-local fill-column 80))
   (setq wolfram-indent 2)
   (with-eval-after-load 'eglot
     (let ((mode '(wolfram-mode :language-id "Wolfram Language"))
