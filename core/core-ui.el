afbd0ab76760df66d87adec5b96b7d6ad9b08c77
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2023-05-06 17:20:43 +0200

    Remove anzu

--- core/core-ui.el
+++ core/core-ui.el
@@ -1,18 +1,5 @@
 ;;; core-ui.el --- User interface setup -*- lexical-binding: t; -*-
 
-;;; Anzu
-
-(use-package anzu
-  :demand
-  :config
-  (setq anzu-search-threshold 1000
-        anzu-replace-threshold 1000)
-  (global-anzu-mode 1))
-
-(use-package evil-anzu
-  :demand
-  :after (evil anzu))
-
 ;;; Cursor
 
 (blink-cursor-mode -1)
