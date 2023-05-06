8eb5a8a05924fb23ea307f27633977aca2b718fd
Pavel Novichkov <9932831+extheo@users.noreply.github.com>
2023-05-06 17:07:15 +0200

    Replace modus-themes -> nano-theme

--- modules/ui/color-theme.el
+++ modules/ui/color-theme.el
@@ -1,10 +1,8 @@
 ;;; ui/color-theme.el -*- lexical-binding: t; -*-
 
-(use-package modus-themes
+(use-package nano-theme
   :demand
+  :custom
+  (nano-light-popout "#DC711A")
   :config
-  (setq modus-themes-diffs 'bg-only
-        modus-themes-lang-checkers '(text-also straight-underline)
-        modus-themes-region '(bg-only no-extend)
-        modus-themes-subtle-line-numbers t)
-  (load-theme 'modus-operandi t))
+  (load-theme 'nano-light t))
