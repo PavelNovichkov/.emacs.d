3aade8bde1febbd8da0e6239db9c523fe38dacac
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2023-09-09 12:39:00 +0200

    Replace my/rename-file-and-buffer -> built-in rename-visited-file

--- modules/bindings.el
+++ modules/bindings.el
@@ -51,22 +51,6 @@ is already narrowed."
           (message "Deleted file %s" filename)
           (kill-buffer))))))
 
-;; From https://github.com/bbatsov/crux.
-(defun my/rename-file-and-buffer ()
-  "Rename current buffer and if the buffer is visiting a file, rename it too."
-  (interactive)
-  (let ((filename (buffer-file-name)))
-    (if (not (and filename (file-exists-p filename)))
-        (rename-buffer (read-from-minibuffer "New name: " (buffer-name)))
-      (let* ((new-name (read-from-minibuffer "New name: " filename))
-             (containing-dir (file-name-directory new-name)))
-        (make-directory containing-dir t)
-        (cond
-         ((vc-backend filename) (vc-rename-file filename new-name))
-         (t
-          (rename-file filename new-name t)
-          (set-visited-file-name new-name t t)))))))
-
 (defun my/split-window-vertically ()
   "Split window vertically, displaying other-buffer in a new
 window, and switch to it."
@@ -147,7 +131,7 @@ no other windows are present."
   "foa" '(find-alternate-file-other-window :which-key "find alternate")
   "fof" '(find-file-other-window :which-key "find")
   "fol" '(find-library-other-window :which-key "find library")
-  "fR" '(my/rename-file-and-buffer :which-key "rename")
+  "fR" '(rename-visited-file :which-key "rename")
   "fr" '(consult-recent-file :which-key "recent")
   "fs" '(save-buffer :which-key "save")
   "fw" '(write-file :which-key "write")
