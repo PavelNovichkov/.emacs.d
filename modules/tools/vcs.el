63d9ecf3cac9a7e5dd9fc2454a26bd98cdfb27e4
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2023-09-12 21:39:56 +0200

    Refactor vcs configuration

--- modules/tools/vcs.el
+++ modules/tools/vcs.el
@@ -3,13 +3,14 @@
 (use-package magit
   :ensure-system-package git
   :commands magit-status
-  :config
-  (setq magit-section-visibility-indicator nil))
+  :custom
+  (magit-section-visibility-indicator nil))
 
 (use-package diff-hl
   :hook (((prog-mode conf-mode) . diff-hl-mode)
          (dired-mode . diff-hl-dired-mode))
+  :custom
+  (diff-hl-disable-on-remote t)
   :config
   (add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
-  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
-  (setq diff-hl-disable-on-remote t))
+  (add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh))
