f65c2be21d242042f4cfd1d96795ba18266a0a79
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2023-09-23 19:30:40 +0200

    Update evil settings

--- core/core-evil.el
+++ core/core-evil.el
@@ -6,21 +6,21 @@
   :custom
   (evil-want-C-d-scroll t)
   (evil-want-C-u-scroll t)
-  (evil-want-C-i-jump nil)
+  (evil-want-C-i-jump t)
   (evil-want-Y-yank-to-eol t)
   (evil-want-keybinding nil)
   (evil-want-integration t)
   (evil-mode-line-format nil)
   (evil-symbol-word-search t)
   (evil-respect-visual-line-mode t)
-  (evil-undo-system 'undo-fu)
+  (evil-undo-system 'undo-redo)
   ;; search and replace
   (evil-ex-search-vim-style-regexp t)
   (evil-ex-substitute-global t)
   (evil-search-module 'evil-search)
   ;; selections
   (evil-ex-visual-char-range t)
-  (evil-want-visual-char-semi-exclusive t)
+  (evil-v$-excludes-newline t)
   (evil-visual-state-cursor 'hollow)
   ;; this does not work:
   ;; (evil-mode t)
