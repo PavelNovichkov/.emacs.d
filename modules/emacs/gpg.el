84c2a40e3439a4b1a35635307eba7c7355725930
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2024-08-10 16:41:59 +0200

    Remove dependency on external library
    Built-in string-trim works just fine.

--- modules/emacs/gpg.el
+++ modules/emacs/gpg.el
@@ -1,8 +1,6 @@
 ;;; emacs/gpg.el -*- lexical-binding: t; -*-
 
-(require 's)
-
 ;; Setup GPG-agent for SSH
 (setenv "SSH_AUTH_SOCK"
-        (s-trim (shell-command-to-string "gpgconf --list-dirs agent-ssh-socket")))
+        (string-trim (shell-command-to-string "gpgconf --list-dirs agent-ssh-socket")))
 (shell-command "gpgconf --launch gpg-agent")
