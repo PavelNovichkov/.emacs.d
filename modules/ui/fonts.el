838cbe8a45068edbafe317187ba78ab697dae542
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2024-08-14 12:04:26 +0200

    Set font for missing characters

--- modules/ui/fonts.el
+++ modules/ui/fonts.el
@@ -10,6 +10,15 @@
  'variable-pitch nil :family "Input Sans Narrow" :height 1.0 :weight 'normal)
 (set-face-attribute 'variable-pitch-text nil :height 1.0)
 
+;; Set font for missing characters (Greek and all characters after Cyrillic).
+
+(defconst my/missing-chars
+  '(greek (#x0530 . #x10FFFF))
+  "List of character ranges to override.")
+
+(dolist (chars my/missing-chars)
+  (set-fontset-font t chars (font-spec :family "JuliaMono") nil 'prepend))
+
 
 ;;; Mixed pitch mode
 
