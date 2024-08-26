cbdfcdeb4d8dab60a078214e43915ab879dad526
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2024-08-26 09:19:50 +0200

    Simplify leader key names

--- core/core-keybindings.el
+++ core/core-keybindings.el
@@ -21,14 +21,14 @@
     :prefix leader-key
     :non-normal-prefix leader-key-non-normal)
   (leader-def
-    "" '("leader prefix" . nil))
+    "" '("leader" . nil))
   (general-create-definer local-leader-def
     :states '(normal insert motion visual emacs)
     :keymaps 'override
     :prefix local-leader-key
     :non-normal-prefix local-leader-key-non-normal)
   (local-leader-def
-    "" '("local-leader prefix" . nil)))
+    "" '("mode" . nil)))
 
 (use-package which-key
   :demand
