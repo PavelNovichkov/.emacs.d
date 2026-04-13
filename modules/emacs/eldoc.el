ca8ee10aa13e7c0d4d8c4593cc9d9cc354d1eaaa
Pavel Novichkov <9932831+PavelNovichkov@users.noreply.github.com>
2026-04-13 15:33:12 +0200

    Add display-buffer rule for eldoc

--- modules/emacs/eldoc.el
+++ modules/emacs/eldoc.el
@@ -5,4 +5,11 @@
   :config
   (setq eldoc-echo-area-display-truncation-message nil
         eldoc-echo-area-prefer-doc-buffer t
-        eldoc-echo-area-use-multiline-p nil))
+        eldoc-echo-area-use-multiline-p nil)
+  (add-to-list
+   'display-buffer-alist
+   '("^\\*eldoc\\*"
+     (display-buffer-in-side-window)
+     (side . right)
+     (slot . 0)
+     (window-width . 80))))
